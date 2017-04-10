d4a791f24277591e0deac7f0b1e8869a0727acca
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-04-10 15:01:42 -0400

    makeTraveler function starting to come together

--- oregontrail.js
+++ oregontrail.js
@@ -1,1 +1,34 @@
 // JavaScript Objects Homework
+
+function makeTraveler (amount, name, isHealthy) {
+    let traveler = [];
+    return {
+        amount: amount,
+        name: name,
+        isHealthy: true,
+    }
+}
+
+let output = {
+    individuals: [
+        makeTraveler(5, 'Ezekiel', true),
+        makeTraveler(7, 'Jebediah', false),
+        makeTraveler(4, 'Sarah', true),
+        makeTraveler(6, 'Mary', false),
+        makeTraveler(11, 'Hezekiah', true),
+    ],
+
+};
+
+console.log(output.individuals[1])
+
+
+
+
+
+
+/*
+function makeWagon ( ) {
+
+}
+*/
