9228cced5276f8aa3db9af639f74383dc859c3cc
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-06-14 02:41:28 -0400

    commit

--- oregontrail.js
+++ oregontrail.js
@@ -25,16 +25,15 @@ function makeWagon(capacity) {
     }
 }
 
-console.log(makeWagon(5));
+console.log(makeWagon(5)); // returns wagon object with capacity of number passed through as an argument and an empty array of passengers.
+
 // hunt(traveler)
 // 50% chance to increase the traveler's food by 100 (successful hunt), and 50% to increase it by 0 (unsuccessful hunt).
 
-// Input: some traveler's object
-// Output: Boolean (hunt success?) 
-
+let Jebediah = { foodamount: 50, isHealthy: true, }
 function hunt(traveler) {
     if (Math.random() > 0.50) {
-        traveler.amount = traveler.amount + 100;
+        traveler.foodamount = traveler.foodamount + 100;
         return true;
     }
     else {
@@ -42,12 +41,29 @@ function hunt(traveler) {
     }
 }
 
-let newoutput = hunt(Jebediah);
-console.log(newoutput);
-console.log(Jebediah);
+console.log(hunt(Jebediah)); // returns 'true' or 'false' based on the success of the hunt when a 
+// traveler object is passed through the function
+
+// DONE.
 
 // eat(traveler)
 // Consumes 20 of the traveler's food. If the traveler doesn't have 20 food, the traveler is no longer healthy.
+
+let George = { foodamount: 50, isHealthy: true, }
+let Sarah = { foodamount: 30, isHealthy: true, }
+
+function eat(traveler) {
+    traveler.foodamount = traveler.foodamount - 20;
+    if (traveler.foodamount < 20) {
+        traveler.isHealthy = false;
+    }
+    return traveler;
+}
+
+console.log(eat(George)); // returns George object with 30 food and isHealthy = true
+console.log(eat(Sarah)); // returns Sarah object with 10 food and isHealthy = false
+
+// DONE.
 
 // join(wagon, traveler)
 // Add the traveler to the wagon if there is space. If there is not enough capacity, don't add them.
